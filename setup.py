235edb0a994e3f641cbd01b9d70b825232496669
Bessy-Mukaria <bessymukaria@gmail.com>
2020-09-22 21:25:17 +0300

    first commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description=': Assuming a role-model of a freelancer hired by 10 Academy, you are to explore the 10 Academy Moodle logs stored in the database together with many other relevant tables.Do analysis and by the end of it build a tableau dashboard.',
+    author='Bessy Mukaria',
+    license='MIT',
+)
